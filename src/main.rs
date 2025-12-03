6b1fef907944b13df2b3a40e647971f27a64a1b2
Iskandar Isabekov <iskandarisabrkov@gmail.com>
2025-12-03 21:09:16 +0500

    Integer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+fn data_typs() {
+    
+    // data types in rust
+    // primitive data types
+    // int float bool char
+
+    // Integers
+    // i8 , i16  i32 , i64 i128 signed integer ( signed that means that both positive and negatives)
+    //u8 , u16 , u32 , u64 , u128 unsigned integer ( unsigned ones are that only positive integers )
+
+    let x: i32 = -42;
+    let y: u32 = 42;
+    
+    println!("Signed integers : {}" , x);
+    println!("Unsigned integers : {}" , y);
+
+
+}
+
 fn main() {
-    println!("Hello world")
+    data_typs();
 }
